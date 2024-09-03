b5a1dd72d46aeb7c103204c21a30964ed00ca304
AbdullahKashif5777 <155015769+AbdullahKashif5777@users.noreply.github.com>
2024-09-03 17:53:50 +0500

    Update EmployeeManager.java

--- pointofsale/EmployeeManager.java
+++ pointofsale/EmployeeManager.java
@@ -55,9 +55,9 @@ public class EmployeeManager {
     }
 
     public static void dummyValues() {
-        employee.add(new Employee("Arslan", "arslan23", "@rslan12", "3456787654567", "03654567632"));
-        employee.add(new Employee("Ahmad", "ahmad43", "@hmad232", "3456907654567", "03652097632"));
-        employee.add(new Employee("Mohaid", "mohaid33", "mo#aid33", "3453492654567", "03871267632"));
-        employee.add(new Employee("Arman", "arman66", "@rman444", "3450983654567", "03654590998"));
+        employee.add(new Employee("Ali", "ali123", "@rslan123", "3456787651267", "03654567632"));
+        employee.add(new Employee("Ahmad", "ahmad123", "@hmad132", "3454507654567", "03652097632"));
+        employee.add(new Employee("hammad", "hammad123", "hamma#33", "3453456654567", "03871267632"));
+        employee.add(new Employee("ismail", "ismail123", "isa#l444", "3450988654567", "03654590998"));
     }
 }
